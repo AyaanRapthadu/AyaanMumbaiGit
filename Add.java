25d41c75ad5b026034bd5d30319da4309c1319f7
Rajesh <rajesh.intell08@gmail.com>
2022-12-28 14:10:07 +0000

    Changes from Singapore Region

--- Add.java
+++ Add.java
@@ -1,9 +1,23 @@
-public class SumOfNumbers1  
-{  
-public static void main(String args[])   
-{  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
-}  
+package com.javaprogramto.engineering.programs;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumbersScanner {
+
+ public static void main(String[] args) {
+
+  // reading input from user
+  Scanner scanner = new Scanner(System.in);
+  System.out.print("Enter First Number : ");
+  int input1 = scanner.nextInt();
+  System.out.print("Enter Second Number : ");
+  int input2 = scanner.nextInt();
+
+  // summing two numbers
+  int output = input1 + input2;
+
+  System.out.println("Scanner example to Sum of two numbers (" + input1 + ", " + input2 + ") = " + output);
+
+ }
+
 }
